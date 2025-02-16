00bfe26723620c2b09735711ad3350a4e7e8a40e
Maclogger <ruckim70@gmail.com>
2025-02-16 04:03:59 +0100

    Správne napojenie vlastnej login stránky, generálny štýl v app.css pridaný.

--- src/resources/js/Components/BasicComponents/SquareButton.vue
+++ src/resources/js/Components/BasicComponents/SquareButton.vue
@@ -1,23 +1,24 @@
 <script setup lang="ts">
+import { Link } from '@inertiajs/vue3';
+
 defineProps<{
     text: string;
     bgColor?: string;
-    href: string;
+    href: string; // URL adresa vygenerovaná Laravel routou
 }>();
 </script>
 
 <template>
-    <a
+    <Link
         :href="href"
         :class="[bgColor ?? 'bg-red-500']"
-        class="aspect-square text-white rounded-2xl h-full mx-4 sm:text-4xl text-3xl px-4 hover_effect flex items-center justify-center no-underline"
+        class="aspect-square text-white rounded-2xl h-full mx-4 sm:text-4xl text-3xl px-4
+               hover_effect flex items-center justify-center no-underline
+               hover:bg-white hover:text-red-500 transition-all duration-300"
     >
         {{ text }}
-    </a>
+    </Link>
 </template>
 
 <style scoped>
-a {
-    text-decoration: none; /* Odstráni defaultné podčiarknutie odkazu */
-}
 </style>
