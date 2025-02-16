1455b6080b3ff2b30d428f474e8bca2e5ad99409
Maclogger <ruckim70@gmail.com>
2025-02-16 03:52:04 +0100

    Používanie mojej login stránky.

--- src/resources/js/Components/BasicComponents/SquareButton.vue
+++ src/resources/js/Components/BasicComponents/SquareButton.vue
@@ -1,21 +1,23 @@
 <script setup lang="ts">
-
 defineProps<{
     text: string;
     bgColor?: string;
+    href: string;
 }>();
-
 </script>
 
 <template>
-    <button
-        v-text="text"
-        :class="[
-            bgColor ?? 'bg-red-500'
-        ]"
-        class="aspect-square block text-white  rounded-2xl h-full mx-4 sm:text-4xl text-3xl px-4 hover_effect"
-    />
+    <a
+        :href="href"
+        :class="[bgColor ?? 'bg-red-500']"
+        class="aspect-square text-white rounded-2xl h-full mx-4 sm:text-4xl text-3xl px-4 hover_effect flex items-center justify-center no-underline"
+    >
+        {{ text }}
+    </a>
 </template>
 
 <style scoped>
+a {
+    text-decoration: none; /* Odstráni defaultné podčiarknutie odkazu */
+}
 </style>
---
+++ src/resources/js/Pages/ChatsPage.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+
+import {User} from "@/types";
+import {ref, onMounted, onUnmounted} from "vue";
+
+var friends: User[];
+
+onMounted(() => {
+    console.log("ahoj");
+})
+
+
+</script>
+
+<template>
+
+</template>
+
+<style scoped>
+
+</style>
